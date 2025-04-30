3679fada347a1914e11dcb20b73d43b6af58a3b0
ChePlusPlus <chephoto@rocketmail.com>
2025-04-30 22:24:01 +0200

    Attempts a background image fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import "./App.css";
 import Background from "./components/Background/Background";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router";
+import bg_light from "./assets/layered-peaks-haikei(2).svg";
+import bg_dark from "./assets/layered-peaks-haikei(1).svg";
 
 function App() {
   const [theme, setTheme] = useState<"light" | "dark" | "system">("light");
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url("${theme === "light" ? bg_light : bg_dark}")`,
+        backgroundSize: "cover",
+        minHeight: "100vh",
+      }}
+    >
       <div data-theme={theme} id="container">
         <Header setTheme={setTheme} />
-        <Background theme={theme} />
+        {/* <Background theme={theme} /> */}
         <Outlet />
       </div>
     </div>
--- src/components/PaymentForm/Container.tsx
+++ src/components/PaymentForm/Container.tsx
@@ -6,7 +6,7 @@ import greyCard from "../../assets/greyCard.svg";
 import stripes from "../../assets/stripes.svg";
 import { cardNumberFormatter } from "../../lib/formatters.ts";
 import "./paymentform.css";
-import { motion, AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 
 let uiState = {
   name: "",
@@ -40,46 +40,27 @@ const Container = () => {
   };
 
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 3,
-          delay: 0,
-          ease: [0, 0.71, 0.2, 1.01],
-        }}
-        exit={{ opacity: 0 }}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          flexBasis: "60vh",
-          justifyContent: "flex-start",
-          margin: "auto",
-          gap: "1rem",
-        }}
-      >
-        <h2 className="main-text">NEED A STYLISH CHECKOUT?</h2>
-        <div className="payment-container flex-box">
-          <div className="cards-container">
-            <motion.div initial={{ x: "-50px" }} animate={{ x: 0 }} className="colorCardContainer">
-              <img src={colorCard} className="colorCard" />
-              <p className="card-number card-text">{cardNumberFormatter(uiState.cardNumber.padEnd(16, "0"))}</p>
-              <p className="card-name card-text">{uiState.name === "" ? "Jane Appleseed" : uiState.name}</p>
-              <p className="date card-text">
-                {uiState.month.padStart(2, "0")}/{uiState.year.padStart(2, "0")}
-              </p>
-            </motion.div>
-            <motion.div initial={{ x: "50px" }} animate={{ x: 0 }} className="greyCardContainer">
-              <img src={greyCard} className="greyCard" />
-              <img src={stripes} className="stripes" />
-              <p className="cvc-ui card-text">{uiState.cvc.padEnd(3, "0")}</p>
-            </motion.div>
-          </div>
-          <div className="payment-content">{!paymentSuccess ? <PaymentForm messages={messages} setMessages={setMessages} clearMessages={clearRecipientsMessages} paymentSucceeded={setPaymentSuccess} updateUI={updateUI} /> : <Success />}</div>
+    <div>
+      <h2 className="main-text">NEED A STYLISH CHECKOUT?</h2>
+      <div className="payment-container flex-box">
+        <div className="cards-container">
+          <motion.div initial={{ x: "-50px" }} animate={{ x: 0 }} className="colorCardContainer">
+            <img src={colorCard} className="colorCard" />
+            <p className="card-number card-text">{cardNumberFormatter(uiState.cardNumber.padEnd(16, "0"))}</p>
+            <p className="card-name card-text">{uiState.name === "" ? "Jane Appleseed" : uiState.name}</p>
+            <p className="date card-text">
+              {uiState.month.padStart(2, "0")}/{uiState.year.padStart(2, "0")}
+            </p>
+          </motion.div>
+          <motion.div initial={{ x: "50px" }} animate={{ x: 0 }} className="greyCardContainer">
+            <img src={greyCard} className="greyCard" />
+            <img src={stripes} className="stripes" />
+            <p className="cvc-ui card-text">{uiState.cvc.padEnd(3, "0")}</p>
+          </motion.div>
         </div>
-      </motion.div>
-    </AnimatePresence>
+        <div className="payment-content">{!paymentSuccess ? <PaymentForm messages={messages} setMessages={setMessages} clearMessages={clearRecipientsMessages} paymentSucceeded={setPaymentSuccess} updateUI={updateUI} /> : <Success />}</div>
+      </div>
+    </div>
   );
 };
 
